8d06395b0bb00c192c4949ddf4758617fa973b5f
Marco-Roa <31865963+Marco-Roa@users.noreply.github.com>
2017-09-20 18:23:08 -0500

    Initial Commit

---
+++ Mi Primera tabla/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mi Primera tabla
+//
+//  Created by Marco Antonio Roa De La Cruz on 9/20/17.
+//  Copyright © 2017 MarcoRoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mi Primera tabla/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mi Primera tabla
+//
+//  Created by Marco Antonio Roa De La Cruz on 9/20/17.
+//  Copyright © 2017 MarcoRoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
