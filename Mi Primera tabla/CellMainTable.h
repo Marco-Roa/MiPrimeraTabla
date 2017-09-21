9e20f6dbb74c2d253c22a05e2534f845aebd8d68
Marco-Roa <31865963+Marco-Roa@users.noreply.github.com>
2017-09-20 19:08:24 -0500

    Add cell infrastructure

---
+++ Mi Primera tabla/CellMainTable.h
@@ -0,0 +1,13 @@
+//
+//  CellMainTable.h
+//  Mi Primera tabla
+//
+//  Created by Marco Antonio Roa De La Cruz on 9/20/17.
+//  Copyright © 2017 MarcoRoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CellMainTable : UITableViewCell
+
+@end
---
+++ Mi Primera tabla/CellMainTable.m
@@ -0,0 +1,24 @@
+//
+//  CellMainTable.m
+//  Mi Primera tabla
+//
+//  Created by Marco Antonio Roa De La Cruz on 9/20/17.
+//  Copyright © 2017 MarcoRoa. All rights reserved.
+//
+
+#import "CellMainTable.h"
+
+@implementation CellMainTable
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
